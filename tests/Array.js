82b1b1d805f2994b2ca6884085defde7618f7c13
Sujit <0011sujitchand@gmail.com>
2026-08-05 15:44:31 +0530

    second commite

--- tests/Array.js
+++ tests/Array.js
@@ -21,3 +21,22 @@ function uniqueElement(){
     console.log(unique);
 }
 uniqueElement();
+
+//Q2. Find Second Largest Number 
+//[45, 89, 23, 89, 56, 12, 78, 34]
+function  FindSecondLargestNumber(arr1){
+    let largest=-Infinity;
+    let secondLargest=-Infinity;
+    for(let i=0;i<arr1.length;i++){
+        if(arr1[i]>largest){
+            secondLargest=largest;
+            largest=arr1[i];
+        }else if(arr1[i]>secondLargest && arr1[i]!==largest){
+            secondLargest=arr1[i];
+        }
+
+    }
+    return secondLargest;
+}
+let arr1=[45, 89, 23, 89, 56, 12, 78, 34];
+console.log(FindSecondLargestNumber(arr1));
